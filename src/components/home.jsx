ca33e7ae5ec8487cbf78b3020e19119c336a18c9
Macman17 <60798387+Macman17@users.noreply.github.com>
2022-05-05 06:09:26 -0700

    update

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -12,15 +12,10 @@ const Home = () => {
                     <img src="/images/img4.png" />
                     <button>Create Your Own Cookie Dough</button>
                     <h3>OR</h3>
-                    <button>Browser Our Ready to Ship Cookies</button>
+                    <Link className="btn btn-lg btn-primary" to="/catalog">Check out our Catalog</Link>
                 </div>
-                <img src="/images/pic6.jpg"  alt="cookies" />
-            </div> 
-
-            <div>
-                <Link className="btn btn-lg btn-primary" to="/catalog"></Link>
             </div>
-            
+                <img src="/images/pic6.jpg"  alt="cookies" />     
         </div>
     );
 
