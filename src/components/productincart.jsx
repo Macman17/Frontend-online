a844637e46c5e4bb19fcdcce01c0a2870a6a4d1b
Macman17 <60798387+Macman17@users.noreply.github.com>
2022-05-07 09:43:03 -0700

    CSS

--- src/components/productincart.jsx
+++ src/components/productincart.jsx
@@ -7,24 +7,25 @@ const ProductInCart = (props) => {
 
   return (
     <div className="productInCart">
-      <div className="displayProduct">
         <img src={"/images/" + props.info.image} alt="" />
         <h4>{props.info.title}</h4>
-        <div className="total">
-          <div className="item">
+        <div className="item">
+            <h6>Price: </h6>
             <label>{props.info.unitPrice.toFixed(2)} </label>
-          </div>
-          <div className="item">
-            <label>{props.info.quantity}</label>
-          </div>
-          <div className="item">
-            <label>$ {getTotal()}</label>
-            <button className="btn btn-lg btn-danger" to="/delete">
-              Delete
-            </button>
-          </div>
         </div>
-      </div>
+        <div className="item">
+        <h6>Qty: </h6>
+        <label>{props.info.quantity}</label>
+        </div>
+        <div className="item">
+            <h6>Total: </h6><br />
+            <label> {getTotal()}</label>
+        </div>
+        <button className="btn btn-lg btn-danger" to="/delete">
+              Delete
+        </button>
+        
+      
     </div>
   );
 };
